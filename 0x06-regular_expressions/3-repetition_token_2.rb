1fe2fffcd5938e8fe58a691205283769ab72b93b
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-02-22 23:07:42 +0000

    Update to match any number of t's

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Accepts one argument and pass it to a regular expression matching method.
-# regex must match: hbtn up to 4 t's.
-puts ARGV[0].scan(/hbt{1,4}n/).join
+# regex must match: hbtn up to any t's.
+puts ARGV[0].scan(/hbt{1,}n/).join
