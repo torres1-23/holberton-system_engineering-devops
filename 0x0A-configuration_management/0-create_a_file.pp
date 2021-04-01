b33b2202e619a708db29abf325931972aabaa4cd
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-01 23:21:39 +0000

    Add puppet script that creates a file with content

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in /tmp.
+
+file { '/tmp/holberton':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+  }
