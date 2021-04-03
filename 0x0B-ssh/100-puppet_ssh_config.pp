fa285b6135c6a374a24f63db91ff5763ce20be74
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-03 22:43:24 +0000

    Updated script

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Set up client SSH configuration file so that you can connect to a server without typing a password.
 
-file_line { 'Add required congig ops':
+file_line { 'Add required config ops':
   path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no
-    IdentityFile ~/.ssh/holberton'
+  line => '    IdentityFile ~/.ssh/holberton
+    PasswordAuthentication no '
 }
