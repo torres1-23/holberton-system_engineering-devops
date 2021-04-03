a2d49ac530b1e170358d40784a46206a456dadd4
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-03 23:11:38 +0000

    Updated script

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
-#Puppet  manifest that edits SSH configuration file
+# Script that changes config ssh file
 
 file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+  line => '    PasswordAuthentication no',
 }
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+  line => '    IdentityFile ~/.ssh/holberton',
 }
