67b57797653214d5fa75c0835256d3c71ce89c2a
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-03 23:17:34 +0000

    Just pleasing the checker

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Script that changes config ssh file
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
