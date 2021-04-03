16dec39c4bcc8c073db33548d5fe6cf93ab4c811
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-03 23:09:57 +0000

    Changed perms

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-# Set up client SSH configuration file so that you can connect to a server without typing a password.
+#Puppet  manifest that edits SSH configuration file
 
-file_line { 'Turn off passwd auth':}
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no'
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton'
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
