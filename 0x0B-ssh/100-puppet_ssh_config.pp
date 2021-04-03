c6df32c1a4b37c002aea35150a6917888221b9f6
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-03 22:46:22 +0000

    Updated script

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,9 @@
 
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
-  line => '   PasswordAuthentication no
-    IdentityFile ~/.ssh/holberton'
+  line => '   PasswordAuthentication no'
+}
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '   IdentityFile ~/.ssh/holberton'
 }
