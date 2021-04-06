ce291f4b371b9ea3fbd430b2da07123f637fe42d
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-06 18:54:13 +0000

    Add puppet manifest to configure an Ubuntu server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,15 +10,16 @@ file { '/var/www/html/index.html':
   content => 'Holberton School'
 }
 
-file_line { '/etc/nginx/sites-available/default':
+file_line { 'Redirection':
   ensure => present,
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
 
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  require   => Package['nginx'],
-  subscribe => File_line['/etc/nginx/sites-available/default']
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Redirection']
 }
