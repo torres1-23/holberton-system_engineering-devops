92a26dc378085568c762b5e0a3f9009925033c4e
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-12 20:45:35 +0000

    Add puppet file to configure a web server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #task advance with pupppet
-exec { 'http header':
+exec { 'nginx install':
   command  => 'sudo apt-get update -y;
 	sudo apt-get install nginx -y;
-	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo $ sed -i '19 i add_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default
 	sudo service nginx restart',
   provider => shell,
 }
