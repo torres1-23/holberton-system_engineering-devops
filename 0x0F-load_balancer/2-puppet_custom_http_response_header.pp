edab64e301089fe9d0b9ea307b61b8d618d0ffe2
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-12 20:25:28 +0000

    Add puppet file to configure a web server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,40 +5,16 @@ package { 'nginx':
   provider => 'apt'
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Holberton School'
-}
-
-file { '/var/www/html/404.html':
-  ensure  => present,
-  content => 'Ceci n'est pas une page'
-}
-
 file_line { 'Custom Header Variable':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;'
-  line   => '        add_header X-Served-By $HOSTNAME;'
-}
-
-file_line { 'Redirection':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
-}
-
-file_line { 'Error':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;'
-  line   => '        error_page 404 /404.html;'
+  line   => '    add_header X-Served-By $HOSTNAME;'
 }
 
 service { 'nginx':
   ensure     => running,
   enable     => true,
   require    => Package['nginx'],
-  subscribe  => File_line['Error']
+  subscribe  => File_line['Custom Header Variable']
 }
