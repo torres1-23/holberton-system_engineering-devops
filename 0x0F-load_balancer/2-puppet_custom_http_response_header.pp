e9b2d757d498e7f7f9dbaabc401da947e6404a83
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-12 20:16:35 +0000

    Add puppet file to configure a web server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,25 +10,25 @@ file { '/var/www/html/index.html':
   content => 'Holberton School'
 }
 
-file_line { 'Redirection':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
-}
-
 file_line { 'Custom Header Variable':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
-  line   => '\tadd_header X-Served-By $HOSTNAME;'
+  after  => 'listen 80 default_server;'
+  line   => '    add_header X-Served-By $HOSTNAME;'
+}
+
+file_line { 'Redirection':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'add_header X-Served-By $HOSTNAME;',
+  line   => '    rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
 
 file_line { 'Error page':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'add_header X-Served-By $HOSTNAME;'
-  line   => '\terror_page 404 /404.html;'
+  after  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  line   => '    error_page 404 /404.html;'
 }
 
 service { 'nginx':
