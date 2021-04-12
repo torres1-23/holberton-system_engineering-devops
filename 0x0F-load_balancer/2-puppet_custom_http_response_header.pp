fe92caaa91846ea791988c77c86e0da28b5aae97
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-12 20:14:21 +0000

    Add puppet file to configure a web server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,7 +14,21 @@ file_line { 'Redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => '    add_header X-Served-By $HOSTNAME;'
+  line   => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+}
+
+file_line { 'Custom Header Variable':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  line   => '\tadd_header X-Served-By $HOSTNAME;'
+}
+
+file_line { 'Error page':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'add_header X-Served-By $HOSTNAME;'
+  line   => '\terror_page 404 /404.html;'
 }
 
 service { 'nginx':
