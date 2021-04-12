bab605369e324f1dfcfe482aceacc095db383507
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-12 20:32:51 +0000

    Add puppet file to configure a web server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,7 +9,7 @@ file_line { 'Custom':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => '       add_header X-Served-By $HOSTNAME;'
+  line   => '    add_header X-Served-By $HOSTNAME;'
 }
 
 service { 'nginx':
