0637bd0bef8c5d546346a21edc1d9842b4d7e9f4
torres1-23 <alejo.torres1247@gmail.com>
2021-05-03 19:44:22 +0000

    Add script that creates a json file an stores an object of user info

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This modules gets information from {JSON} Placeholder
+API to list information about an employee, and stores it on
+a json format in a file.
+
+Usage:
+    Accepts one argument, the id of the employee to list info.
+    Execute this module like this:
+    <python3 2-export_to_JSON.py <id of user>>
+    json file will have the name: <user_id>.json
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_info_str = ""
+    user_info_list = []
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    user_info = requests.get(url)
+    user_name = user_info.json()['username']
+    url_todo = url + '/todos'
+    user_todos_info = requests.get(url_todo)
+    todos_list = user_todos_info.json()
+    for dic in todos_list:
+        user_info_list.append(
+            {'task': dic['title'], 'completed': dic['completed'],
+                'username': user_name})
+        user_info_dict = {id: user_info_list}
+    with open("{}.json".format(id), "w") as file:
+        json.dump(user_info_dict, file)
