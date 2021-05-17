d463b56114307b9ffd4ad13f35dc622f1afe1e00
torres1-23 <alejo.torres1247@gmail.com>
2021-05-17 20:45:58 +0000

    Update script

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -38,7 +38,7 @@ def count_words(subreddit, word_list, word_count={}, after=""):
         return count_words(subreddit, word_list, word_count, after)
     if word_count:
         word_count_sorted = dict(sorted(word_count.items(),
-                                    key=lambda item: (-item[1], item[0])))
+                                 key=lambda item: (-item[1], item[0])))
         for key, value in word_count_sorted.items():
             print("{}: {}".format(key, value))
     else:
